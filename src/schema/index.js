23f7eace21cdf3ccd74ef6439c1b58c4218f093f
Dustin Schau <dustinschau@gmail.com>
2018-04-30 14:27:00 -0500

    chore: add a fullName resolver

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,20 +1,21 @@
-import { makeExecutableSchema } from 'graphql-tools';
-import axios from 'axios';
-import config from 'config';
-import qs from 'query-string';
+import { makeExecutableSchema } from "graphql-tools";
+import axios from "axios";
+import config from "config";
+import qs from "query-string";
 
-const BASE_URL = config.get('restURL');
+const BASE_URL = config.get("restURL");
 
 const schema = `
   type User {
     id: ID!
-    name: Name
+    name: Name!
     email: String!
   }
 
   type Name {
     first: String!
     last: String!
+    fullName: String!
   }
 
   type Comment {
@@ -57,51 +58,63 @@ const resolvers = {
   Query: {
     comments(root, { id, ...rest }) {
       const params = qs.stringify(rest);
-      return axios.get(`${BASE_URL}/posts/${id}/comments/?${params}`)
+      return axios
+        .get(`${BASE_URL}/posts/${id}/comments/?${params}`)
         .then(response => response.data.comments);
     },
     comment(root, { id, commentId }) {
-      return axios.get(`${BASE_URL}/posts/${id}/comments/${commentId}`)
+      return axios
+        .get(`${BASE_URL}/posts/${id}/comments/${commentId}`)
         .then(response => response.data);
     },
     posts(root, args) {
       const params = qs.stringify(args);
-      return axios.get(`${BASE_URL}/posts?${params}`)
+      return axios
+        .get(`${BASE_URL}/posts?${params}`)
         .then(response => response.data.posts);
     },
     post(root, { id }) {
       const url = `${BASE_URL}/posts/${id}`;
-      return axios.get(url)
-        .then(response => response.data);
+      return axios.get(url).then(response => response.data);
     },
     users(root, args) {
       const params = qs.stringify(args);
-      return axios.get(`${BASE_URL}/users?${params}`)
+      return axios
+        .get(`${BASE_URL}/users?${params}`)
         .then(response => response.data.users);
     },
     user(root, { id }) {
-      return axios.get(`${BASE_URL}/users/${id}`)
+      return axios
+        .get(`${BASE_URL}/users/${id}`)
         .then(response => response.data);
     }
   },
   Mutation: {
     addPost(root, args) {
-      return axios.post(`${BASE_URL}/posts`, args)
+      return axios
+        .post(`${BASE_URL}/posts`, args)
         .then(response => response.data);
     },
     updatePost(root, { id, ...body }) {
-      return axios.put(`${BASE_URL}/posts/${id}`, body)
+      return axios
+        .put(`${BASE_URL}/posts/${id}`, body)
         .then(response => response.data);
+    }
+  },
+  Name: {
+    fullName(name) {
+      return `${name.first} ${name.last}`;
     }
   },
   Post: {
     comments(post, args) {
       const params = qs.stringify(args);
-      return axios.get(`${BASE_URL}/posts/${post.id}/comments?${params}`)
+      return axios
+        .get(`${BASE_URL}/posts/${post.id}/comments?${params}`)
         .then(response => response.data.comments);
     }
   }
-}
+};
 
 export const SCHEMA = makeExecutableSchema({
   typeDefs: schema,
